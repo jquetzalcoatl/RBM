261649691f8451915de28e18ef02ba31b508cc58
jquetzalcoatl <j.toledo.mx@gmail.com>
2023-05-19 10:47:52 -0700

    organized flow

--- utils/train.jl
+++ utils/train.jl
@@ -8,7 +8,7 @@ include("en.jl")
 include("tools.jl")
 
 # Training function
-function train( ; epochs=50, nv=28*28, nh=100, batch_size=100, lr=0.001, t=10, plotSample=false, annealing=false, β=1, PCD=true, gpu_usage = false, t_samp=100, optType="SGD")
+function train( ; epochs=50, nv=28*28, nh=100, batch_size=100, lr=0.001, t=10, plotSample=false, annealing=false, β=1, PCD=true, gpu_usage = false, t_samp=100, num=40, optType="SGD")
     
     rbm, J, m, hparams = initModel(; nv, nh, batch_size, lr, t, gpu_usage, optType)
     if optType=="Adam"
@@ -55,7 +55,7 @@ function train( ; epochs=50, nv=28*28, nh=100, batch_size=100, lr=0.001, t=10, p
         if epoch % 1 == 0
             @info epoch, m.enList[end]/(hparams.nv+hparams.nh), m.ΔwList[end], m.ΔaList[end], m.ΔbList[end], β
             if plotSample
-                genSample(rbm, J, hparams, m; num = 4, β, t=t_samp, dev)
+                genSample(rbm, J, hparams, m; num, β, t=t_samp, dev)
             end
         end
         if annealing
